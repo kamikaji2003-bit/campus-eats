8a5ece229fbfdb41bc829b57e3abd8a8993920ae
kamikaji2003-bit <kami.kaji2003@gmail.com>
2026-09-11 22:39:03 +0600

    Unit IV Lab 3: PostgreSQL connection, Restaurant and MenuItem models, restaurant-specific menus

--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -1,3 +1,5 @@
-exports.getAbout = (req, res) => {
-  res.render('about', { title: 'About Campus Eats' });
+const Restaurant = require('../models/Restaurant');
+exports.getHome = async (req, res) => {
+ const restaurants = await Restaurant.getAllRestaurants();
+ res.render('index', { title: 'Campus Eats', restaurants });
 };
